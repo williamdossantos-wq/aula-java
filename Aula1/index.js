00130763fdb7be390bb82a8814ef44486d0a6335
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-04-08 12:21:24 -0300

    js1

---
+++ Aula1/index.js
@@ -0,0 +1,5 @@
+console.log('hello word');   //string = texto
+console.log('william dos "santos"');
+console.log(`hello word`);
+console.log(123456, 13.34, "william dos santos"); // number = número
+console.log('Meu nome é "william". Estou aprendendo Javascript às' ,10, 'da manhã.')
