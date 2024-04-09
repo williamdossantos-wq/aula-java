85442ca90159aa817653ec40423018af5b928429
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-04-09 11:59:41 -0300

    parte2

---
+++ Aula2/comentários.js
@@ -0,0 +1,11 @@
+// isso não aparece
+console.log('ola mundo'); //aqui tambem não
+console.log('linha' ,1);
+console.log('linha' ,2);
+/*
+ja vi nas aulas 
+html e css
+ */
+console.log('linha' ,3);
+console.log('linha' ,4);
+//aqui tambem não
---
+++ Aula3/html-vs-node.js
@@ -0,0 +1,3 @@
+console.log('Olá mundo');
+console.log('ola munde');
+console.log('esse trecho vai aparecer no html')
