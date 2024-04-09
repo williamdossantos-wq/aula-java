be9b6ff12312e91ac1d7ee22fc793267f2765a72
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-04-09 12:15:16 -0300

    rev

--- Aula3/html-vs-node.js
+++ Aula3/html-vs-node.js
@@ -1,3 +1,4 @@
 console.log('Olá mundo');
 console.log('ola munde');
 console.log('esse trecho vai aparecer no html')
+console.log('No novo testamento, a palavra concupiscência é traduzida do grego (epithumia), que também significa o desejo forte de algo.');
