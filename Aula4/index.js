9aa8b6ee2ae0262b485db6eac8c231d9f8292353
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-04-10 12:24:58 -0300

    revparte3

---
+++ Aula4/index.js
@@ -0,0 +1,13 @@
+let nome = 'João'; // let = variavel
+let momi; // Declarou a variavel
+nomi = 'qualquer valor'; //inicializando a variavel
+//Não podemos criar variaveis com palavras reservadas. ex; let let, let console.
+//Variaveis precisam ter nomes siginificativos. variaveis não podem causar confusão.  
+// variaveis não podem começa com numero
+//variaveis não podem ter espaços ou traços, causa falha no codigo. em vez disto use camelCase ex; let nomeCompletodoCliente = 'joão erique'
+//NÃO UTILIZE VAR, UTILIZE LET.
+console.log(nomi)
+console.log('Em 2000' ,nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2010.');
+console.log('Maria teve um filho com' ,nome, 'em 2015.');
+console.log('O filho de' ,nome, 'se chama Eduardo.');
