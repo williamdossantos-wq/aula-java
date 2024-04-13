ee4f88484aaf598bea0130a7d402e98cb15da267
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-04-13 11:33:20 -0300

    parte-6

---
+++ Aula7/index.js
@@ -0,0 +1,3 @@
+var nome = 'william';
+var nome = 'Otávio';
+nome = 'luiz'; //NÃO FAÇA ISSO!!!!!
---
+++ Aula8/index.js
@@ -0,0 +1,14 @@
+const nome ='william'; // string
+const nome1 ="william";// string
+const nome2 =`william`;// string
+const num1 = 1;// number
+const num2 = 10.10;// number
+let nomeNãodefinido;// undefined = não aponta para lugar nenhum na mémoria
+const nomeNãodefinido2 = null;//nulo -> não aponta para lugar nenhum na mémoria
+const aprovado = true;// boolean = true , false (lógico)
+const a = 2;
+const b = a;
+const c = 3;
+const d = 'c';
+console.log( a , b);
+console.log(c ,d);
