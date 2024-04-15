f21caaf3534e86d688e5b74ad400fcd0583cebf5
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-04-15 12:43:37 -0300

    rev-parte-9

---
+++ Aula10/js/index.js
@@ -0,0 +1,8 @@
+
+const num1 = prompt('Digite um numero');
+const num2 = prompt('Digite outro numero');
+num1 = Number(num1);
+num2 = Number(num2);
+const resultado = num1 + num2;
+alert(o resultado foi...)
+console.log(resultado);
