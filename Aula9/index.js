a8f926cdb0523fae9ce87210f23d13a70509e48b
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-04-15 11:59:29 -0300

    parte-7

--- Aula9/index.js
+++ Aula9/index.js
@@ -1,3 +1,59 @@
 /*
-
+ Aritméticos
+    () 
+    + adição / concatenação
+    - / * 
+    ** potenciação
+    % resto da divião
+    INCREMENTO = ++
+    DECREMENTO = --
+    Operadores de atribuição
+    NAN - NOT A NUMBER / parseInt (inteiro), parseFloat (decimais) / Number 
 */
+const num1 = 5;
+const num = 3;
+console.log( num1 + num)
+console.log( num1 * num)
+console.log( num1 / num)
+console.log( num1 - num)
+console.log( num1 ** num)
+console.log( num1 % num)
+console.log( num1 + num * ( num1 - num / num))
+
+let contador = 4;
+contador++;
+let contadora = 1;
+let contadore = 10;
+console.log( contador);
+console.log( contadora++);
+console.log( contadora)
+console.log( ++contadora)
+console.log( --contadore);
+console.log( --contadore);
+
+const passo = 2;
+let adição = 0;
+adição = adição + passo;
+console.log( adição)
+adição = adição + passo;
+console.log( adição)
+adição = adição + passo;
+console.log( adição)
+adição = adição + passo;
+console.log( adição)
+
+let adição1 = 2;
+adição1 *= 5;
+adição1 *= 5;
+console.log(adição1)
+
+const num3 = 1;
+const num4 = 'will';
+console.log( num3 * num4)
+const num5 = parseInt('32');
+console.log( typeof num5)
+console.log( num5 + num3)
+const num6 = parseFloat('1.2');
+console.log(num5 + num6);
+const num7 = Number('3');
+console.log(num7 + num3);
