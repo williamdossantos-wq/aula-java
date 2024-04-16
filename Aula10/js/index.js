509907099804a3b20dc9fff68e0417ff692547c2
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-04-16 00:34:42 -0300

    parte-8

--- Aula10/js/index.js
+++ Aula10/js/index.js
@@ -1,8 +1,9 @@
+let num1 = prompt('n1');
+let num2 = prompt('n2');
 
-const num1 = prompt('Digite um numero');
-const num2 = prompt('Digite outro numero');
 num1 = Number(num1);
 num2 = Number(num2);
 const resultado = num1 + num2;
-alert(o resultado foi...)
-console.log(resultado);
+
+alert('O resultado da sua conta foi:' + resultado);
+
