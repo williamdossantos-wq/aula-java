9f5c6cdc1c6a140fa0b3e1fb7d81e6b1c4b57d33
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-04-16 11:17:21 -0300

    parte-10

---
+++ Aula11/index.js
@@ -0,0 +1,18 @@
+//               012345678911
+let umaString = "um texto";
+                 //01234567891123456789212345678931234
+let umastring = 'o rato roeu a roupa do rei de roma.';
+console.log(umaString[4]);
+console.log(umaString .concat(' em um lindo dia.' ));
+console.log(umaString + ' em um lindo dia.');
+console.log(`${umaString} em um lindo dia.`);
+console.log(umaString.indexOf('texto'));
+console.log(umaString.indexOf('x', 2));
+console.log(umaString.lastIndexOf('t',8));
+console.log(umaString.match(/[a-z]/g));
+console.log(umaString.match(/[a-z]/));
+console.log(umaString.search(/[a-z]/));
+console.log(umaString.replace('um', 'texto'));
+console.log(umastring.replace(/r/g, 'l'));
+console.log(umaString.length);
+console.log(umastring.length);
