8bcbd22329956dc66dd6ab559fef9ce2495ac83f
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-04-19 11:27:58 -0300

    parte-12

---
+++ Aula14/index.js
@@ -0,0 +1,16 @@
+let num1 = 9.5547482;
+let num2 = Math.floor(num1); //arredonda para baixo.
+let num3 = Math.ceil(num1); //arredonda para cima.
+let num4 = Math.round(num1); // arredonda para cima e para baixo dependendo da quantidade que falta.
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(Math.max(1,2,34,-78,3**7, 598)); // vai falar o maior número na sequência.
+console.log(Math.min(12,836,563,-735,9366, -6*37)); // vai falar o menor número na sequência.
+console.log(Math.random()); //me dar um número aleatório .
+const aleatorio = Math.round(Math.random() * (20 - 10) + 5);
+console.log(aleatorio);
+let num5 =16;
+console.log(num5 **0.5); //raiz quadrada.
+console.log(100 / 0); // vai dar com verdadeiro!!!!
+
