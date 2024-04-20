d53ccc5dd5234e67342edb6b1e133cd693695563
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-04-19 22:37:01 -0300

    rev-parte-12

--- Aula13/index.js
+++ Aula13/index.js
@@ -5,17 +5,17 @@ let num2 = 20.937357287992;
 //console.log(num1.toString() + num2);
 //num1 = num1.toString();
 //console.log(num1.toString(2));
-//console.log(num2.toFixed(2)); 
+console.log(num2.toFixed(3)); 
 
-/*console.log(Number.isInteger(num1)); isInteger vai verificar se o número é inteiro: se for vai dar true, se não for vai dar false.
-console.log(Number.isInteger(num2));
-*/
+//console.log(Number.isInteger(num1)); //isInteger vai verificar se o número é inteiro: se for vai dar true, se não for vai dar false.
+//console.log(Number.isInteger(num2));
 
-/*let temp = num1 * 'ola';
+
+let temp = num1 * 'ola';
 let tempo = num1 * '9';
-console.log(Number.isNaN(temp)); isNan vai verificar se a conta é invalida com true ou é valida com false.
+console.log(Number.isNaN(temp)); //isNan vai verificar se a conta é invalida com true ou é valida com false.
 console.log(Number.isNaN(tempo));
-*/
+/*
  let num3 = 0.7;
  let num4 = 0.1;
  num3 += num4; //0.8
@@ -34,3 +34,4 @@ console.log(Number.isNaN(tempo));
 num3 = parseFloat(num3.toFixed(2)); // parsetFloat ou Number resolve o problema.
 console.log( num3);
 console.log(Number.isInteger(num3));
+*/
