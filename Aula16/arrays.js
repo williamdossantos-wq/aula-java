41179d6f061483e13150556b8a11a34f6f7daacb
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-04-23 11:39:45 -0300

    parte-14

---
+++ Aula16/arrays.js
@@ -0,0 +1,32 @@
+  //            0123
+  const nome = 'luiz';
+  console.log(nome[3]);  
+  //                   0        1        2
+    const alunos = ['joão' , 'maria' , 'jose'];
+    //console.log(typeof alunos); // array são objetos
+    //console.log(alunos instanceof Array); // se for um array vai dar trun ,se não vai dar false
+
+    //delete alunos[2]; //deleta um nome , mas o indice fica em branco
+    //const removido = alunos.pop(); //remove o ultimo indice
+    //const removidoin = alunos.shift(); // remove o primeiro indice
+    //alunos.unshift ('matheus'); // adiciona no inicio
+    //alunos.unshift ('chico'); //adiciona no inicio
+
+    /*
+    alunos[0] = 'roge'; // altera 
+    alunos.push('matheus'); //adiciona no fim
+    alunos.push('guilheme') ; // adiciona no fim
+    alunos.push('lucas');
+*/
+    /*
+    alunos[3] = 'charles'; // adincionando
+    alunos[alunos.length] = 'matheus'; //adiciona no fim
+    alunos[alunos.length] = 'guilheme';
+    alunos[alunos.length] = 'lucas';
+    */
+
+    //console.log(alunos.length);
+   // console.log(removidoin); 
+    //console.log(removido);
+    console.log(alunos);
+   //console.log(alunos[2]);
