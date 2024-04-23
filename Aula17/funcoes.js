870c8be8878de1157424ce7e17d7b805c9c02d18
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-04-23 12:45:33 -0300

    parte-15

---
+++ Aula17/funcoes.js
@@ -0,0 +1,21 @@
+function soma(x, y){
+const resultado = x * y;
+return resultado
+}
+
+console.log(soma(2, 4));
+console.log(soma(3, 6));
+
+const raiz = function(b) {
+    return b ** 0.5;
+};
+console.log(raiz(25));
+console.log(raiz(38));
+console.log(raiz(676));
+
+const vezes = (v) => v *20;
+
+console.log(vezes(20)) ;
+console.log(vezes(30)) ;
+console.log(vezes(40)) ;
+console.log(vezes(50)) ;
