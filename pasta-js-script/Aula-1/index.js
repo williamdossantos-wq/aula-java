1a29a005d0c4c61f437b5a8612bccfc9a4539828
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-04-30 11:59:47 -0300

    parte-1-js-script

---
+++ pasta-js-script/Aula-1/index.js
@@ -0,0 +1,24 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade (valor) ******** não use!!!!!!!
+=== igualdade restrita (valor e tipo) 
+!= diferente (valor) ********* não use!!!!!!
+!== diferente restrito (valor e tipo)
+*/
+
+console.log(10 > 9);
+console.log(10 > 10);
+console.log(10 >= 9);
+console.log(9 >= 10);
+console.log(10 < 9);
+console.log(10 < 10);
+console.log(10 <= 10);
+console.log(10 <= 9);
+console.log(10 === 10);
+console.log(10 === '10');
+console.log(10 !== 10);
+console.log(10 !== '10');
---
+++ pasta-js-script/Aula-2/index.js
@@ -0,0 +1,12 @@
+/*
+Operadoeres Lógicos
+&& -> AND -> E -> todas as expressões precisam ser verdadeiras  para retornar verdadeira = true
+|| -> OR -> OU 
+! -> NOT -> NÃO -> negaçoes
+*/
+const expressaoAND = true && true && true && true;
+const expressaoOR = false || true || false || false;
+
+
+console.log(expressaoOR);
+
