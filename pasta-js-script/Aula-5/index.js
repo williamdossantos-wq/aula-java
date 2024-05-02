bd3c5fd2ebd51d46decbef2643f593d3f111232f
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-05-02 11:27:02 -0300

    parte-2

---
+++ pasta-js-script/Aula-5/index.js
@@ -0,0 +1,14 @@
+const numero = 6;
+if (numero >= 0 && numero <= 5){
+    console.log('O numero esta entre zero e cinco');
+ }else if (numero >= 6 && numero <= 9){
+    console.log('O numero esta entre seis e nove')
+} else if(numero >= 10 && numero <= 20){
+    console.log('O numero esta entre dez e vinte')
+}
+else if (1 === 1){
+    console.log( 'literal')
+    
+    } else {
+    console.log('O numero não esta entre zero e vinte')
+}
