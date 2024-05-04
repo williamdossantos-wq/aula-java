8acb71766a56aa97781aec20eee39da8ca6bd1f7
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-05-04 10:46:53 -0300

    parte-3-rev

--- pasta-js-script/Modelo/assets/js/index.js
+++ pasta-js-script/Modelo/assets/js/index.js
@@ -1,1 +1,17 @@
-alert(1)
+function meuIMC (){
+const form = document.querySelector('form');
+const resultado = document.querySelector('resultado');
+function recebeEventoform (evento){
+    evento.preventDefault()
+
+const peso = form.querySelector('.peso-imc');
+const altura = form.querySelector('.altura-imc');
+
+
+
+}
+
+resultado.innerHTML += 
+form.addEventListener('submit',recebeEventoform);
+}
+meuIMC()
