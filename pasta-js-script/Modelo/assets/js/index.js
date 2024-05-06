aee2f45dfedb575306f14eaad6d751c9a6b08c24
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-05-06 11:55:25 -0300

    parte-3-rev1

--- pasta-js-script/Modelo/assets/js/index.js
+++ pasta-js-script/Modelo/assets/js/index.js
@@ -1,17 +1,16 @@
-function meuIMC (){
-const form = document.querySelector('form');
-const resultado = document.querySelector('resultado');
-function recebeEventoform (evento){
-    evento.preventDefault()
-
-const peso = form.querySelector('.peso-imc');
-const altura = form.querySelector('.altura-imc');
+function meuImc (){
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
 
 
+    function recebeEventoform (evento){
+        evento.preventDefault();
 
+        const peso = form.querySelector('.peso-imc');
+        const altura = form.querySelector('.altura-imc');
+    
+
+    }
+        form.addEventListener('submit', recebeEventoform);
 }
-
-resultado.innerHTML += 
-form.addEventListener('submit',recebeEventoform);
-}
-meuIMC()
+meuImc()
