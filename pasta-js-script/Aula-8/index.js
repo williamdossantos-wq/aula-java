d0e2507c10a2b2a16e7a63fe421dc0fc514d28f5
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-05-17 11:16:32 -0300

    parte-8

--- pasta-js-script/Aula-8/index.js
+++ pasta-js-script/Aula-8/index.js
@@ -1,33 +1,79 @@
-const data = new Date('2003-06-07');
-const diadaSemana = data.getDay() ;
+const data = new Date();
+const diadaSemana = data.getDay();
 let diaSemanaTexto;
 
 switch (diadaSemana) {
     case 0:
-    diaSemanaTexto = 'Domingo';
-    break;
-     case 1:
-    diaSemanaTexto = 'Segunda';
-    break;
-     case 2:
-    diaSemanaTexto = 'Terça';
-    break;
-     case 3:
-    diaSemanaTexto = 'Quarta';
-    break;
-     case 4:
-    diaSemanaTexto = 'Quinta';
-    break;
-     case 5:
-    diaSemanaTexto = 'Sexta';
-    break;
-     case 6:
-    diaSemanaTexto = 'Sabado';
-    break;
-     case 7:
-    diaSemanaTexto = 'Domingo';
-    break;
-}
+        diaSemanaTexto = 'Domingo';
+        break;
+    case 1:
+        diaSemanaTexto = 'Segunda-feira';
+        break;
+    case 2:
+        diaSemanaTexto = 'Terça-feira';
+        break;
+    case 3:
+        diaSemanaTexto = 'Quarta-feiara';
+        break;
+    case 4:
+        diaSemanaTexto = 'Quinta-feira';
+        break;
+    case 5:
+        diaSemanaTexto = 'Sexta-feira';
+        break;
+    case 6:
+        diaSemanaTexto = 'Sabado';
+        break;
+};
+
+const seuMes = data.getMonth();
+let mesTexto;
+
+switch (seuMes) {
+    case 0:
+        mesTexto = 'janeiro';
+        break;
+    case 1:
+        mesTexto = 'fevereiro';
+        break;
+    case 2:
+        mesTexto = 'março';
+        break;
+    case 3:
+        mesTexto = 'abril';
+        break;
+    case 4:
+        mesTexto = 'maio';
+        break;
+    case 5:
+        mesTexto = 'junho';
+        break;
+    case 6:
+        mesTexto = 'julho';
+        break;
+    case 7:
+        mesTexto = 'agosto';
+        break;
+    case 8:
+        mesTexto = 'setembro';
+        break;
+    case 9:
+        mesTexto = 'outubro';
+        break;
+    case 10:
+        mesTexto = 'novembro';
+        break;
+    case 11:
+        mesTexto = 'dezembro';
+        break;
+
+};
+
+const dia = data.getDate();
+const ano = data.getFullYear();
+const hora = data.getHours();
+const minutos = data.getMinutes();
+
 
 
 /*if (diadaSemana === 0){
@@ -46,4 +92,4 @@ switch (diadaSemana) {
     diaSemanaTexto = 'Sabado';
 }*/
 
-console.log(diadaSemana, diaSemanaTexto);
+console.log( `${diaSemanaTexto} , ${dia} de  ${mesTexto} de ${ano}  ${hora}:${minutos}`); 
--- pasta-js-script/Aula-9/js-css/index.js
+++ pasta-js-script/Aula-9/js-css/index.js
@@ -1,11 +1,86 @@
 const horasProjeto = document.querySelector('.horasprojeto');
 
 
-const seila = 'olaaa';
-function mostrarHora (horasProjeto){
+
+const data = new Date();
+const diadaSemana = data.getDay();
+let diaSemanaTexto;
+
+switch (diadaSemana) {
+    case 0:
+        diaSemanaTexto = 'Domingo';
+        break;
+    case 1:
+        diaSemanaTexto = 'Segunda-feira';
+        break;
+    case 2:
+        diaSemanaTexto = 'Terça-feira';
+        break;
+    case 3:
+        diaSemanaTexto = 'Quarta-feiara';
+        break;
+    case 4:
+        diaSemanaTexto = 'Quinta-feira';
+        break;
+    case 5:
+        diaSemanaTexto = 'Sexta-feira';
+        break;
+    case 6:
+        diaSemanaTexto = 'Sabado';
+        break;
+};
+
+const seuMes = data.getMonth();
+let mesTexto;
+
+switch (seuMes) {
+    case 0:
+        mesTexto = 'janeiro';
+        break;
+    case 1:
+        mesTexto = 'fevereiro';
+        break;
+    case 2:
+        mesTexto = 'março';
+        break;
+    case 3:
+        mesTexto = 'abril';
+        break;
+    case 4:
+        mesTexto = 'maio';
+        break;
+    case 5:
+        mesTexto = 'junho';
+        break;
+    case 6:
+        mesTexto = 'julho';
+        break;
+    case 7:
+        mesTexto = 'agosto';
+        break;
+    case 8:
+        mesTexto = 'setembro';
+        break;
+    case 9:
+        mesTexto = 'outubro';
+        break;
+    case 10:
+        mesTexto = 'novembro';
+        break;
+    case 11:
+        mesTexto = 'dezembro';
+        break;
+
+};
+
+const dia = data.getDate();
+const ano = data.getFullYear();
+const hora = data.getHours();
+const minutos = data.getMinutes();
+const segundos = data.getSeconds();
+
+//console.log( `${diaSemanaTexto} , ${dia} de  ${mesTexto} de ${ano}  ${hora}:${minutos}`); 
 
 
-}
-horasProjeto.innerHTML = `<p>Nome; ${seila} </p>`;
+horasProjeto.innerHTML = `${diaSemanaTexto} , ${dia} de  ${mesTexto} de ${ano}  ${hora}:${minutos}:${segundos}`;
 
-mostrarHora();
