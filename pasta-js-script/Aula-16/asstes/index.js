3dd3c2be6443766935c665a33979d438e9483203
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-05-24 11:39:05 -0300

    parte-13

--- pasta-js-script/Aula-16/asstes/index.js
+++ pasta-js-script/Aula-16/asstes/index.js
@@ -1,3 +1,11 @@
 const paragrafo = document.querySelector('.paragrafo');
 const ps = paragrafo.querySelectorAll('p');
 
+const estilos = getComputedStyle(document.body);
+const backgroundBody = estilos.backgroundColor;
+
+
+for (let p of ps){
+p.style.backgroundColor = '#FFE4C4';
+p.style.color = '#DC143C';
+}
---
+++ pasta-js-script/Aula-17/index.js
@@ -0,0 +1,21 @@
+/*const nome = 'william';
+let i = 0;
+
+while (i < nome.length){
+    console.log(nome[i]);
+    i++
+};*/
+//console.log('para aqui');
+function random (min, max){
+    const r = Math.random() * (max - min) + min;
+    return Math.floor (r)
+}
+const min = 1;
+const max = 20;
+
+let rar = random(min, max);
+
+while ( rar !== 5){
+    rar = random(min, max);
+    console.log(rar);
+}
