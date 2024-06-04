678e67c9e5b3fd2174df9cfa61de0b1f217396bf
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-06-04 12:21:50 -0300

    parte-21

--- pasta-js-script/Aula-23/js-css/index.js
+++ pasta-js-script/Aula-23/js-css/index.js
@@ -1,30 +1,40 @@
+function relogio(){
 const relogio = document.querySelector('.relogio');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
-iniciar.addEventListener('click', function (event){
-   relogio.innerHTML = setInterval(Meutimer,1000);
-   function Meutimer(){
-       const agora = new Date ();
-       const hora = agora.getHours();
-   const minutos = agora.getMinutes();
-   const segundos = agora.getSeconds();
-       relogio.innerHTML =`${hora}:${minutos}:${segundos}`
-   }
-})
-pausar.addEventListener('click', function (event){
-    alert('cliquei no pausar')
-})
-zerar.addEventListener('click', function (event){
-    alert('cliquei no zarar')
-})
-
-setInterval(Meutimer,1000);
-function Meutimer(){
-    const agora = new Date ();
-    const hora = agora.getHours();
-const minutos = agora.getMinutes();
-const segundos = agora.getSeconds();
-    relogio.innerHTML =`${hora}:${minutos}:${segundos}`
+function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+    })
 }
+let segundos = 0;
+let timer;
+function iniciaRelogio() {
+    timer = setInterval(function () {
+        segundos++;
+        relogio.innerHTML = criaHoraDosSegundos(segundos)
+    }, 1000)
+};
+
+document.addEventListener('click', function (e) {
+    const el = e.target;
+    if (el.classList.contains('iniciar')) {
+        relogio.classList.remove('pausado')
+        clearInterval(timer);
+        iniciaRelogio()
+    }
+
+    if (el.classList.contains('pausar')) {
+        relogio.classList.add('pausado')
+        clearInterval(timer);
+    }
+    if (el.classList.contains('zerar')) {
+        clearInterval(timer);
+        segundos = 0;
+        relogio.innerHTML = '00:00:00'
+    }
+})
+};
+relogio();
+
 
