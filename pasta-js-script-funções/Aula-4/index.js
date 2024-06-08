11f4f69d0afbc17a20f9026ab2f177ee10c63676
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-06-08 12:25:42 -0300

    parte-3

---
+++ pasta-js-script-funções/Aula-4/index.js
@@ -0,0 +1,29 @@
+//71.Escopo lexico
+
+/*const nome = 'william';
+function falaNome () {
+    console.log(nome);
+
+}
+falaNome();
+
+function usaFalaNome() {
+    falaNome()
+}
+usaFalaNome();*/
+
+//72.Closures
+
+/*
+function retornaFuncao() {
+    const nome = 'william';
+    return function() {
+        return nome;
+    };
+};
+const funcao = retornaFuncao();
+console.log(funcao)
+*/
+
+//73.Funções de callback
+
