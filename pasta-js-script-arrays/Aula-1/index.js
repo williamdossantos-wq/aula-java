ba96c2e01ae339905ebe8c6a4ddab78e69183053
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-06-24 11:03:46 -0300

    Update index.js

--- pasta-js-script-arrays/Aula-1/index.js
+++ pasta-js-script-arrays/Aula-1/index.js
@@ -1,4 +1,19 @@
 const nomes = ['william', 'julia', 'fenarda', 'diego'];
 nomes[2] = 'joao';
+const remopri = nomes.shift();
 delete nomes[2];
+const novo = [...nomes];
+const removido = novo.pop();
+const novo2 = nomes.slice(0, 3)
+nomes.push('jose')
+nomes.unshift('wallece')
+console.log(nomes.length);
+console.log(novo)
+console.log(removido)
+console.log(remopri)
 console.log(nomes)
+console.log(novo2)
+
+const nomes3 = 'will rebeca diego';
+const nomes4 = nomes3.split(' ');
+console.log(nomes4);
