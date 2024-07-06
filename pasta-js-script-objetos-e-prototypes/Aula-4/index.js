c9984b5f2bbed29ebaeb0c14a2288430f4c0ebfc
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-07-06 12:08:32 -0300

    obj-pt5

--- pasta-js-script-objetos-e-prototypes/Aula-4/index.js
+++ pasta-js-script-objetos-e-prototypes/Aula-4/index.js
@@ -14,16 +14,44 @@ console.dir(pessoa2.nomeCompleto());
 
 //new Object -> Object.prototype
 const objA = {
-chaveA: 'A'
+chaveA: 'Ana'
 //_proto_ = Object.prototype
 };
 const objB = {
-    chaveB: 'B'
+    chaveB: 'Bruno'
 };
 const objC = {
-    chaveC: 'C'
+    chaveC: 'Caio'
 };
 Object.setPrototypeOf(objC, objB);
+Object.setPrototypeOf(objB,objA)
 
-console.log(objC.chaveB);
+console.log(objC.chaveA);
 
+function  Produto(item, preco) {
+    this.item = item;
+    this.preco = preco;
+};
+Produto.prototype.desconto = function(percetual) {
+this.preco = this.preco - (this.preco * (percetual / 100))
+};
+Produto.prototype.aumento = function(aumento) {
+    this.preco = this.preco + (this.preco * (aumento / 100))
+    };
+const p1 = new Produto('camiseta', 10)
+p1.aumento(10)
+//p1.desconto(10)
+const p2 = {
+    nome:'chinelo',
+    preco:20
+}
+
+Object.setPrototypeOf(p2, Produto.prototype);
+p2.aumento('200')
+
+console.log(p1);
+console.log(p2);
+const p3 = Object.create(Produto.prototype);
+p3.preco = 40;
+p3.aumento(10);
+console.log(p3);
---
+++ pasta-js-script-objetos-e-prototypes/Aula-5/index.js
@@ -0,0 +1,14 @@
+//95.Heranças
+//Produto = aumento , desconto
+//camiseta = cor, caneca = material
+function Produto(nome, preco) {
+    this.nome = nome;
+    this.preco = preco;
+};
+Produto.prototype.aumento = function(quantia) {
+    this.preco += quantia;
+};
+Produto.prototype.desconto = function(quantia) {
+    this.preco -= quantia;
+};
+
