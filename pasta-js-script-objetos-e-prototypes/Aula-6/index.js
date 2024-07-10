378427af3c54a5704288b539c7388e071436e99f
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-07-10 12:40:54 -0300

    obj-pt6.2

--- pasta-js-script-objetos-e-prototypes/Aula-6/index.js
+++ pasta-js-script-objetos-e-prototypes/Aula-6/index.js
@@ -27,8 +27,29 @@ get: function() {
 
 Validacpf.prototype.valida = function() {
 if(typeof this.cpfLimpo === 'undefined') return false;  
+if(this.cpfLimpo.length !== 11) return false;
+
+const cpfParcial = this.cpfLimpo.slice(0,-2)
+const digito1 = this.criaDigito(cpfParcial);
+console.log(digito1)
+
     return true
-}
+};
+
+
+Validacpf.prototype.criaDigito = function(cpfParcial) {
+const cpfArray = Array.from(cpfParcial);
+
+let regressivo = cpfArray.length + 1;
+const total = cpfArray.reduce(function(ac, val) {
+ac += (regressivo * Number(val))
+    regressivo --;
+    return ac;
+}, 0);
+const digito = 11 - (total % 11);
+return digito > 9 ? 0 : digito}
+
+
 const cpf = new Validacpf('705.484.450-52');
 
 console.log(cpf.cpfLimpo)
