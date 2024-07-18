0b680ef017bdefcf54ff404851df78da58552da7
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-07-18 13:34:09 -0300

    class-pt4

---
+++ pasta-js-classes/Aula-5/index.js
@@ -0,0 +1,55 @@
+//705.484.450-52 070.987.720-03
+class Validacpf{
+    constructor(cpfEnviado){
+Object.defineProperty(this,'cpfLimpo',{
+  writable:false,
+  enumerable:true,
+  configurable:false,
+value: cpfEnviado.replace(/\D+/g,'')
+    }
+);
+}
+
+
+valida() {
+    
+        if(typeof this.cpfLimpo === 'undefined') return false;
+        if(this.cpfLimpo.length !== 11) return false;
+        if(this.isSequecia()) return false;
+
+            const cpfParcial = this.cpfLimpo.slice(0,-2);
+            const digito1 = this.criaDigito(cpfParcial);
+            const digito2 = this.criaDigito(cpfParcial + digito1);
+            const novocpf = cpfParcial + digito1 + digito2;
+            return novocpf === this.cpfLimpo;
+ 
+}};
+
+Validacpf.prototype.criaDigito = class{
+    constructor(cpfParcial) {
+const cpfArray = array.from(cpfParcial);
+let regressivo = cpfArray.length + 1;
+const total = cpfArray.reduce(function(ac, val) {
+    ac += (regressivo * Number(val));
+    regressivo --;
+    return ac;
+}, 0);
+const digito = 11 - (total % 11);
+return digito > 9 ? '0' : String(digito)}
+
+}
+Validacpf.prototype.isSequecia = class {
+    constructor() {
+        const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+        return sequencia === this.cpfLimpo;
+    }
+}
+
+
+const cpf = new Validacpf('705.484.450-52');
+
+if(cpf.Valida()) {
+console.log('cpfValido')
+} else (
+    console.log('cpfInvalido')
+)
