ad67e36ef89f0084e477457bdd2a2b712d30518b
williamdossantos-wq <110804154+williamdossantos-wq@users.noreply.github.com>
2024-07-22 21:57:26 -0300

    class-pt7

--- pasta-js-classes/Aula-6/css-js/index.js
+++ pasta-js-classes/Aula-6/css-js/index.js
@@ -12,6 +12,27 @@ this.formulario.addEventListener('submit', e => {
     handleSubmit(e) {
     e.preventDefault();
     const camposValidos = this.camposSaoValidos();
+    const senhasValidas = this.senhasSaoValidas();
+    if(camposValidos && senhasValidas) {
+        alert('formulario enviado')
+        this.formulario.submit();
+    }
+}
+senhasSaoValidas() {
+let valid = true;
+const senha = this.formulario.querySelector('.senha');
+const repetirSenha = this.formulario.querySelector('.repetir-senha')
+if(senha.value !== repetirSenha.value) {
+valid = false;
+this.criaError(senha,'Campos senha e repetir senha precisam ser iguais');
+this.criaError(repetirSenha,'Campos senha e repetir senha precisam ser iguais')
+}
+if(senha.value.length < 6 || senha.value.length > 12 ){
+    valid = false;
+    this.criaError(senha,'A senha precisa ter entre 6 e 12 caracteres')
+}
+
+return valid;
 }
 camposSaoValidos() {
 let valid = true;
@@ -28,7 +49,12 @@ for(let campo of this.formulario.querySelectorAll('.Validado')) {
    if(campo.classList.contains('cpf')){
     if(!this.validaCpf(campo)) valid = false;
    }
+   if(campo.classList.contains('usuario')){
+    if(!this.validaUsuario(campo)) valid = false;
+   }
+  
 }
+return valid;
 }
 validaCpf(campo) {
     const cpf = new Validacpf(campo.value)
@@ -37,6 +63,20 @@ this.criaError(campo,'CPF invalido')
 return false;
     }
     return true;
+}
+validaUsuario(campo){
+    const usuario =  campo.value;
+    let valid = true;
+    if(usuario.length < 3 || usuario.length > 12) {
+        this.criaError(campo, 'Usuario precisa ter entre 3 e 12 caracteres')
+       valid = false;
+    }
+
+    if(!usuario.match(/^[a-zA-Z0-9]+$/g)) {
+        this.criaError(campo, 'Nome de Usuario precisa conter apenas letras e/ou numeros  ')
+       valid = false;
+    }
+return valid;
 }
 criaError(campo, msg){
 const div = document.createElement('div');
